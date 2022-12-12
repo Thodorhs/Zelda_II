adaefa8a3b6b62c8399c156add4ac3cd3a1fe680
Thodoris Pontzouktzidis <thodp@ics.forth.gr>
2022-12-12 15:20:53 +0200

    uploaded files

---
+++ hy454/CS454/CS454/Source.cpp
@@ -0,0 +1,88 @@
+
+
+#include "SDL.h"
+#undef main
+#include <iostream>
+
+
+//don't do this, this is just an example
+SDL_Renderer* renderer;
+SDL_Window* window;
+bool isRunning;
+bool fullscreen;
+void handleEvents();
+void update();
+void render();
+
+
+//please don't put all your code in main like I did.
+int main() {
+
+	fullscreen = false;
+	int flags = 0;
+	flags = SDL_WINDOW_RESIZABLE;
+	if (fullscreen) {
+		flags = flags | SDL_WINDOW_FULLSCREEN;
+	}
+	if (SDL_Init(SDL_INIT_EVERYTHING) == 0) {
+		std::cout << "Subsystems Initialized!\n";
+
+		window = SDL_CreateWindow("Test Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 500, 500, flags);
+		if (window) {
+			std::cout << "Window Created!\n";
+			SDL_SetWindowMinimumSize(window, 100, 100);
+		}
+		renderer = SDL_CreateRenderer(window, -1, 0);
+		if (renderer) {
+			SDL_SetRenderDrawColor(renderer, 121, 121, 121, 255);
+			std::cout << "Renderer created!\n";
+			SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+			isRunning = true;
+		}
+
+	}
+
+	while (isRunning) {
+		handleEvents();
+		update();
+		render();
+	}
+
+	//frees memory associated with renderer and window
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);	//error here
+	SDL_Quit();
+
+
+	return 0;
+}
+
+//handles any events that SDL noticed.
+void handleEvents() {
+	//the only event we'll check is the  SDL_QUIT event.
+	SDL_Event event;
+	SDL_PollEvent(&event);
+
+
+	switch (event.type) {
+	case SDL_QUIT:
+		isRunning = false;
+		break;
+	default:
+		break;
+	}
+}
+
+//simple render function
+void render() {
+	SDL_SetRenderDrawColor(renderer, 121, 121, 121, 255);
+	SDL_RenderClear(renderer);
+
+	//your stuff to render would typically go here.
+	SDL_RenderPresent(renderer);
+}
+
+//simple update function
+void update() {
+	//if things could update the code would go in here.
+}
