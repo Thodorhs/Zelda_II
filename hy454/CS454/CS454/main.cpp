17aa1c5006a150ff1ab5118c9a6269128126a194
NikolasIliakis <csd4375@LAPTOP-S57HAKS3.localdomain>
2022-12-24 17:26:06 +0200

    new project -nikos

--- hy454/CS454/CS454/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "ZeldaApp.h"
-
-
-int main() {
-	ZeldaApp App;
-	App.Initialise();
-	App.Run();
-}
---
+++ hy454/csd454/CS454/ZeldaApp.h
@@ -0,0 +1,10 @@
+#include "App.h"
+
+class ZeldaApp :app::App {
+	public:
+		void Initialise(void);
+		void Load(void);
+		void Clear(void);
+		void Run(void);
+		void RunIteration(void);
+};
